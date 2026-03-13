af9a43497981e6c48b054ab200b7156d7b0e4f58
dignekabayiza <dignekabayiza250@gmail.com>
2026-03-13 07:49:48 -0700

    Add 4-print_rev.c and update main.h

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to string
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+int len;
+int i;
+
+len = _strlen(s);
+for (i = len - 1; i >= 0; i--)
+_putchar(s[i]);
+_putchar('\n');
+}
