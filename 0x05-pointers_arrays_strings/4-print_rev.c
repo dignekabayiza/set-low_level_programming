b53841c483ce9a1d354ce8b148e64516c6b5ae6e
dignekabayiza <dignekabayiza250@gmail.com>
2026-03-13 07:54:51 -0700

    Fix 4-print_rev.c Betty style

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,11 @@
  */
 void print_rev(char *s)
 {
-int len;
-int i;
+	int len;
+	int i;
 
-len = _strlen(s);
-for (i = len - 1; i >= 0; i--)
-_putchar(s[i]);
-_putchar('\n');
+	len = _strlen(s);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
