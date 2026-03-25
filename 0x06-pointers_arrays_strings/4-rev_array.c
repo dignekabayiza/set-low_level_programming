1a268af4c25441ec5334bcd7f01870f923091a99
dignekabayiza <dignekabayiza250@gmail.com>
2026-03-25 03:22:02 -0700

    Add 4-rev_array.c and update main.h

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: pointer to array
+ * @n: number of elements
+ *
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+int tmp;
+
+for (i = 0; i < n / 2; i++)
+{
+tmp = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = tmp;
+}
+}
